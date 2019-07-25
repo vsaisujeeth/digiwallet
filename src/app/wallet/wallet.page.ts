dd0dcda5e217ab9a7df06739b226c2dd02562f7a
vsaisujeeth <vsaisujeeth10@yahoo.com>
2019-07-26 01:23:28 +0530

    before hosting

--- src/app/wallet/wallet.page.ts
+++ src/app/wallet/wallet.page.ts
@@ -3,6 +3,7 @@ import { Wallet } from '../models/wallet.model';
 import { IonItemSliding } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { EthereumService } from '../services/ethereum.service';
 
 @Component({
   selector: 'app-wallet',
@@ -11,18 +12,47 @@ import { AuthService } from '../services/auth.service';
 })
 export class WalletPage implements OnInit {
 
+  balance;
+  wallet: Wallet;
+
   wallets: Wallet[];
   constructor(
+    public eth: EthereumService,
     private auth: AuthService,
     private route: Router
     ) { }
 
+    async getBalance()
+    {
+      this.balance = await this.eth.balance(this.auth.presentUser.wallets[0].address);
+      console.log(this.balance);
+      this.auth.presentUser.wallets[0].balance = this.balance  / 1000000000000000000 ;
+      this.auth.updateUser();
+    }
+ 
+    async getBalanceOf(id)
+    {
+      this.wallet = this.auth.presentUser.wallets.find(data => data.id == id);
+      this.balance = await this.eth.balanceOf(this.auth.presentUser.wallets[0].address, this.wallet.address);
+      console.log(this.balance);
+      this.auth.presentUser.wallets.find(data => data.id == id).balance = this.balance/1e18;
+      this.auth.updateUser();
+    }
   ngOnInit() {
     this.wallets = this.auth.presentUser.wallets;
+
   }
 
   ionViewWillEnter() {
     this.wallets = this.auth.presentUser.wallets;
+    this.getBalance();
+
+    let i = 1;
+    for( i = 1; i < this.wallets.length; i++)
+    {
+      this.getBalanceOf(i);
+    }
+
    }
 
   onSlide(id, sliding: IonItemSliding) {
